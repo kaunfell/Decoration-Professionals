b2dc917c9b6a2ec0622cacdb5f8dcb5a0d8400e8
kaunfell <jenny.kaunistunturi@gmail.com>
2023-12-13 14:09:02 +0200

    modified projects area

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 
 
-
+// Sidenavigation, open and close
 function openNav()  {
     document.getElementById("sidenav").style.width = "230px";
 }
 
 function closeNav()  {
     document.getElementById("sidenav").style.width = "0";
+
+}
+
+
+
+
+// Servicesbox, open and close
+function showServices() {
+    document.getElementById('servicesbox').style.display = 'flex';
+
 }
 
 function closeServices()  {
     document.getElementById("servicesbox").style.display = "none";
-}
+}  
 
-S
-function showServices() {
 
-    document.getElementById('servicesbox').style.display = 'flex';
+
+// Projectpictures, zoom
+function zoomImg(element) {
+    element.classList.toggle('zoomed');
+    
 
 }
